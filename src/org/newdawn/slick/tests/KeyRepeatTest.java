857103c96c931d01296d2e6fcd992579fbe4ca32
kevin <kevin@825ea778-2522-0410-b336-eb26e3730c59>
2007-05-11 21:29:49 +0000

    Removed unused import

--- src/org/newdawn/slick/tests/KeyRepeatTest.java
+++ src/org/newdawn/slick/tests/KeyRepeatTest.java
@@ -4,7 +4,6 @@ import org.newdawn.slick.AppGameContainer;
 import org.newdawn.slick.BasicGame;
 import org.newdawn.slick.GameContainer;
 import org.newdawn.slick.Graphics;
-import org.newdawn.slick.Image;
 import org.newdawn.slick.Input;
 import org.newdawn.slick.SlickException;
 
