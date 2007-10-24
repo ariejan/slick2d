d20bc12f44cdd9486b84ef4ad9060a9167fcd375
kevin <kevin@825ea778-2522-0410-b336-eb26e3730c59>
2007-10-24 19:49:44 +0000

    Removed unused import

--- src/org/newdawn/slick/tests/GeomUtilTileTest.java
+++ src/org/newdawn/slick/tests/GeomUtilTileTest.java
@@ -7,7 +7,6 @@ import org.newdawn.slick.BasicGame;
 import org.newdawn.slick.Color;
 import org.newdawn.slick.GameContainer;
 import org.newdawn.slick.Graphics;
-import org.newdawn.slick.Input;
 import org.newdawn.slick.SlickException;
 import org.newdawn.slick.geom.Circle;
 import org.newdawn.slick.geom.GeomUtil;
