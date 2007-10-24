1e2e384b5e434a3c9b3754a66b6535c17f44bcf5
kevin <kevin@825ea778-2522-0410-b336-eb26e3730c59>
2007-10-24 12:53:18 +0000

    Currently failing Tile union test

---
+++ src/org/newdawn/slick/tests/GeomUtilTileTest.java
@@ -0,0 +1,197 @@
+package org.newdawn.slick.tests;
+
+import java.util.ArrayList;
+
+import org.newdawn.slick.AppGameContainer;
+import org.newdawn.slick.BasicGame;
+import org.newdawn.slick.Color;
+import org.newdawn.slick.GameContainer;
+import org.newdawn.slick.Graphics;
+import org.newdawn.slick.Input;
+import org.newdawn.slick.SlickException;
+import org.newdawn.slick.geom.GeomUtil;
+import org.newdawn.slick.geom.GeomUtilListener;
+import org.newdawn.slick.geom.Polygon;
+import org.newdawn.slick.geom.Shape;
+import org.newdawn.slick.geom.Vector2f;
+
+/**
+ * A test to try shape building from multiple tiles
+ * 
+ * @author Kevin Glass
+ */
+public class GeomUtilTileTest extends BasicGame implements GeomUtilListener {
+	/** The shape we're cutting out of */
+	private Shape source;
+	/** The shape we're cutting */
+	private Shape cut;
+	/** The resulting shape */
+	private Shape[] result;
+	
+	/** The util under test */
+	private GeomUtil util = new GeomUtil();
+	
+	/** The original list of shapes */
+	private ArrayList original = new ArrayList();
+	/** The original list of shapes */
+	private ArrayList combined = new ArrayList();
+	
+	/** The list of intersection points */
+	private ArrayList intersections = new ArrayList();
+	/** The list of used points */
+	private ArrayList used = new ArrayList();
+	
+	/**
+	 * Create a simple test
+	 */
+	public GeomUtilTileTest() {
+		super("GeomUtilTileTest");
+	}
+
+	/**
+	 * Perform the cut
+	 */
+	public void init() {
+		int[][] map = new int[][] {
+				{0,0,0,0,0,0,0,0,0,0},
+				{0,1,1,0,0,0,0,0,0,0},
+				{0,1,0,0,0,0,0,0,0,0},
+				{0,1,1,0,0,0,0,0,0,0},
+				{0,0,0,0,0,0,0,0,0,0},
+				{0,0,0,0,0,0,0,0,0,0},
+				{0,0,0,0,0,0,0,0,0,0},
+				{0,0,0,0,0,0,0,0,0,0},
+				{0,0,0,0,0,0,0,0,0,0},
+				{0,0,0,0,0,0,0,0,0,0},
+		};
+		for (int x=0;x<10;x++) {
+			for (int y=0;y<10;y++) {
+				if (map[y][x] != 0) {
+					Polygon poly = new Polygon();
+					poly.addPoint(x*32, y*32);
+					poly.addPoint((x*32)+32, y*32);
+					poly.addPoint((x*32)+32, (y*32)+32);
+					poly.addPoint(x*32, (y*32)+32);
+					original.add(poly);
+					//original.add(new Rectangle(x*32,y*32,33,33));
+				}
+			}
+		}
+		
+		combined = combineImpl(original);
+	}
+	
+	private ArrayList combine(ArrayList shapes) {
+		ArrayList last = shapes;
+		ArrayList current = shapes;
+		boolean first = true;
+		
+		while ((current.size() != last.size()) || (first)) {
+			first = false;
+			last = current;
+			current = combineImpl(current);
+		}
+		
+		return current;
+	}
+	
+	private ArrayList combineImpl(ArrayList shapes) {
+		ArrayList result = new ArrayList(shapes);
+		
+		for (int i=0;i<shapes.size();i++) {
+			Shape first = (Shape) shapes.get(i);
+			for (int j=i+1;j<shapes.size();j++) {
+				Shape second = (Shape) shapes.get(j);
+				
+				Shape[] joined = util.union(first,second);
+				if (joined.length == 1) {
+					result.remove(first);
+					result.remove(second);
+					result.add(joined[0]);
+					return result;
+				}
+			}
+		}
+		
+		return result;
+	}
+	
+	/**
+	 * @see BasicGame#init(GameContainer)
+	 */
+	public void init(GameContainer container) throws SlickException {
+		util.setListener(this);
+		init();
+		container.setVSync(true);
+	}
+
+	/**
+	 * @see BasicGame#update(GameContainer, int)
+	 */
+	public void update(GameContainer container, int delta)
+			throws SlickException {
+		if (container.getInput().isKeyPressed(Input.KEY_SPACE)) {
+			intersections.clear();
+			used.clear();
+			combined = combineImpl(combined);
+			System.out.println(intersections.size());
+		}
+	}
+	
+	/**
+	 * @see org.newdawn.slick.Game#render(GameContainer, Graphics)
+	 */
+	public void render(GameContainer container, Graphics g)
+			throws SlickException {
+		g.setColor(Color.green);
+		for (int i=0;i<original.size();i++) {
+			Shape shape = (Shape) original.get(i);
+			g.draw(shape);
+		}
+		g.setColor(Color.white);
+		for (int i=0;i<intersections.size();i++) {
+			Vector2f pt = (Vector2f) intersections.get(i);
+			g.drawOval(pt.x-3, pt.y-3, 7,7);
+		}
+		g.setColor(Color.yellow);
+		for (int i=0;i<used.size();i++) {
+			Vector2f pt = (Vector2f) used.get(i);
+			g.fillOval(pt.x-1, pt.y-1, 3,3);
+		}
+
+		g.translate(0,320);
+		g.setColor(Color.white);
+		
+		for (int i=0;i<combined.size();i++) {
+			Shape shape = (Shape) combined.get(i);
+			g.draw(shape);
+		}
+		
+	}
+	
+	/**
+	 * Entry point to our test
+	 * 
+	 * @param argv The arguments passed to the test
+	 */
+	public static void main(String[] argv) {
+		try {
+			AppGameContainer container = new AppGameContainer(new GeomUtilTileTest());
+			container.setDisplayMode(800,600,false);
+			container.start();
+		} catch (SlickException e) {
+			e.printStackTrace();
+		}
+	}
+
+	public void pointExcluded(float x, float y) {
+	}
+
+	public void pointIntersected(float x, float y) {
+		intersections.add(new Vector2f(x,y));
+	}
+
+	public void pointUsed(float x, float y) {
+		used.add(new Vector2f(x,y));
+	}
+}
