cc770cc473d9d2079d704e38012c8ff7eeac646e
kevin <kevin@825ea778-2522-0410-b336-eb26e3730c59>
2007-11-11 20:07:36 +0000

    TrueType font support relying on Java 2D from Jimmy

---
+++ src/org/newdawn/slick/TrueTypeFont.java
@@ -0,0 +1,343 @@
+package org.newdawn.slick;
+
+import java.awt.Color;
+import java.awt.FontMetrics;
+import java.awt.Graphics2D;
+import java.awt.RenderingHints;
+import java.awt.image.BufferedImage;
+import java.io.IOException;
+
+import org.newdawn.slick.opengl.Texture;
+import org.newdawn.slick.opengl.renderer.Renderer;
+import org.newdawn.slick.opengl.renderer.SGL;
+import org.newdawn.slick.util.BufferedImageUtil;
+
+/**
+ * A TrueType font implementation for Slick
+ * 
+ * @author James Chambers (Jimmy)
+ * @author Jeremy Adams (elias_naur)
+ * @author Kevin Glass (kevglass)
+ */
+public class TrueTypeFont implements org.newdawn.slick.Font {
+	/** The renderer to use for all GL operations */
+	private static final SGL GL = Renderer.get();
+
+	/** Array that holds necessary information about the font characters */
+	private IntObject[] charArray = new IntObject[256];
+
+	/** Boolean flag on whether AntiAliasing is enabled or not */
+	private boolean antiAlias;
+
+	/** Font's size */
+	private int fontSize = 0;
+
+	/** Font's height */
+	private int fontHeight = 0;
+
+	/** Texture used to cache the font characters */
+	private Texture fontTexture;
+
+	/** Default font texture width */
+	private float textureWidth = 512.0f;
+
+	/** Default font texture height */
+	private float textureHeight = 512.0f;
+
+	/** A reference to Java's AWT Font that we create our font texture from */
+	private java.awt.Font font;
+
+	/** The font metrics for our Java AWT font */
+	private FontMetrics fontMetrics;
+
+	/**
+	 * This is a special internal class that holds our necessary information for
+	 * the font characters. This includes width, height, and where the character
+	 * is stored on the font texture.
+	 */
+	private class IntObject {
+		/** Character's width */
+		public int width;
+
+		/** Character's height */
+		public int height;
+
+		/** Character's stored x position */
+		public int storedX;
+
+		/** Character's stored y position */
+		public int storedY;
+	}
+
+	/**
+	 * Constructor for the TrueTypeFont class Pass in the preloaded standard
+	 * Java TrueType font, and whether you want it to be cached with
+	 * AntiAliasing applied.
+	 * 
+	 * @param font
+	 *            Standard Java AWT font
+	 * @param antiAlias
+	 *            Whether or not to apply AntiAliasing to the cached font
+	 */
+	public TrueTypeFont(java.awt.Font font, boolean antiAlias) {
+		this.font = font;
+		this.fontSize = font.getSize();
+		this.antiAlias = antiAlias;
+
+		createPlainSet();
+	}
+
+	/**
+	 * Create a standard Java2D BufferedImage of the given character
+	 * 
+	 * @param ch
+	 *            The character to create a BufferedImage for
+	 * 
+	 * @return A BufferedImage containing the character
+	 */
+	private BufferedImage getFontImage(char ch) {
+		// Create a temporary image to extract the character's size
+		BufferedImage tempfontImage = new BufferedImage(1, 1,
+				BufferedImage.TYPE_INT_ARGB);
+		Graphics2D g = (Graphics2D) tempfontImage.getGraphics();
+		if (antiAlias == true) {
+			g.setRenderingHint(RenderingHints.KEY_ANTIALIASING,
+					RenderingHints.VALUE_ANTIALIAS_ON);
+		}
+		g.setFont(font);
+		fontMetrics = g.getFontMetrics();
+		int charwidth = fontMetrics.charWidth(ch);
+
+		if (charwidth <= 0) {
+			charwidth = 1;
+		}
+		int charheight = fontMetrics.getHeight();
+		if (charheight <= 0) {
+			charheight = fontSize;
+		}
+
+		// Create another image holding the character we are creating
+		BufferedImage fontImage;
+		fontImage = new BufferedImage(charwidth, charheight,
+				BufferedImage.TYPE_INT_ARGB);
+		Graphics2D gt = (Graphics2D) fontImage.getGraphics();
+		if (antiAlias == true) {
+			gt.setRenderingHint(RenderingHints.KEY_ANTIALIASING,
+					RenderingHints.VALUE_ANTIALIAS_ON);
+		}
+		gt.setFont(font);
+
+		gt.setColor(Color.WHITE);
+		int charx = 0;
+		int chary = 0;
+		gt.drawString(String.valueOf(ch), (charx), (chary)
+				+ fontMetrics.getAscent());
+
+		return fontImage;
+
+	}
+
+	/**
+	 * Create and store the font
+	 */
+	private void createPlainSet() {
+		try {
+			BufferedImage imgTemp = new BufferedImage(512, 512,
+					BufferedImage.BITMASK);
+			Graphics2D g = (Graphics2D) imgTemp.getGraphics();
+
+			int rowHeight = 0;
+			int positionX = 0;
+			int positionY = 0;
+
+			for (int i = 0; i < 256; i++) {
+				char ch = (char) i;
+				BufferedImage fontImage = getFontImage(ch);
+
+				IntObject newIntObject = new IntObject();
+
+				newIntObject.width = fontImage.getWidth();
+				newIntObject.height = fontImage.getHeight();
+
+				if (positionX + newIntObject.width >= 512) {
+					positionX = 0;
+					positionY += rowHeight;
+					rowHeight = 0;
+				}
+
+				newIntObject.storedX = positionX;
+				newIntObject.storedY = positionY;
+
+				if (newIntObject.height > fontHeight) {
+					fontHeight = newIntObject.height;
+				}
+
+				if (newIntObject.height > rowHeight) {
+					rowHeight = newIntObject.height;
+				}
+
+				// Draw it here
+				g.drawImage(fontImage, positionX, positionY, null);
+
+				positionX += newIntObject.width;
+
+				charArray[i] = newIntObject;
+
+				fontImage = null;
+			}
+
+			fontTexture = BufferedImageUtil
+					.getTexture(font.toString(), imgTemp);
+
+		} catch (IOException e) {
+			System.err.println("Failed to create font.");
+			e.printStackTrace();
+		}
+
+	}
+
+	/**
+	 * Draw a textured quad
+	 * 
+	 * @param drawX
+	 *            The left x position to draw to
+	 * @param drawY
+	 *            The top y position to draw to
+	 * @param drawX2
+	 *            The right x position to draw to
+	 * @param drawY2
+	 *            The bottom y position to draw to
+	 * @param srcX
+	 *            The left source x position to draw from
+	 * @param srcY
+	 *            The top source y position to draw from
+	 * @param srcX2
+	 *            The right source x position to draw from
+	 * @param srcY2
+	 *            The bottom source y position to draw from
+	 */
+	private void drawQuad(int drawX, int drawY, int drawX2, int drawY2,
+			int srcX, int srcY, int srcX2, int srcY2) {
+		int DrawWidth = drawX2 - drawX;
+		int DrawHeight = drawY2 - drawY;
+		float TextureSrcX = srcX / textureWidth;
+		float TextureSrcY = srcY / textureHeight;
+		float SrcWidth = srcX2 - srcX;
+		float SrcHeight = srcY2 - srcY;
+		float RenderWidth = (SrcWidth / textureWidth);
+		float RenderHeight = (SrcHeight / textureHeight);
+
+		GL.glTexCoord2f(TextureSrcX, TextureSrcY);
+		GL.glVertex2f(drawX, drawY);
+		GL.glTexCoord2f(TextureSrcX, TextureSrcY + RenderHeight);
+		GL.glVertex2f(drawX, drawY + DrawHeight);
+		GL.glTexCoord2f(TextureSrcX + RenderWidth, TextureSrcY + RenderHeight);
+		GL.glVertex2f(drawX + DrawWidth, drawY + DrawHeight);
+		GL.glTexCoord2f(TextureSrcX + RenderWidth, TextureSrcY);
+		GL.glVertex2f(drawX + DrawWidth, drawY);
+	}
+
+	/**
+	 * Get the width of a given String
+	 * 
+	 * @param whatchars
+	 *            The characters to get the width of
+	 * 
+	 * @return The width of the characters
+	 */
+	public int getWidth(String whatchars) {
+		int totalwidth = 0;
+		IntObject intObject = null;
+		int currentChar = 0;
+		for (int i = 0; i < whatchars.length(); i++) {
+			currentChar = whatchars.charAt(i);
+			if (currentChar < 256) {
+				intObject = charArray[currentChar];
+				totalwidth += intObject.width;
+			}
+		}
+		return totalwidth;
+	}
+
+	/**
+	 * Get the font's height
+	 * 
+	 * @return The height of the font
+	 */
+	public int getHeight() {
+		return fontHeight;
+	}
+
+	/**
+	 * Get the height of a String
+	 * 
+	 * @return The height of a given string
+	 */
+	public int getHeight(String HeightString) {
+		return fontHeight;
+	}
+
+	/**
+	 * Get the font's line height
+	 * 
+	 * @return The line height of the font
+	 */
+	public int getLineHeight() {
+		return fontHeight;
+	}
+
+	/**
+	 * Draw a string
+	 * 
+	 * @param x
+	 *            The x position to draw the string
+	 * @param y
+	 *            The y position to draw the string
+	 * @param whatchars
+	 *            The string to draw
+	 * @param color
+	 *            The color to draw the text
+	 */
+	public void drawString(float x, float y, String whatchars,
+			org.newdawn.slick.Color color) {
+		color.bind();
+		fontTexture.bind();
+
+		IntObject intObject = null;
+		int charCurrent;
+
+		GL.glBegin(SGL.GL_QUADS);
+
+		int totalwidth = 0;
+		for (int i = 0; i < whatchars.length(); i++) {
+			charCurrent = whatchars.charAt(i);
+			if (charCurrent < 256) {
+				intObject = charArray[charCurrent];
+
+				drawQuad((int) (x + totalwidth), (int) y,
+						(int) (x + totalwidth + intObject.width),
+						(int) (y + intObject.height), intObject.storedX,
+						intObject.storedY, intObject.storedX + intObject.width,
+						intObject.storedY + intObject.height);
+				totalwidth += intObject.width;
+			}
+		}
+
+		GL.glEnd();
+	}
+
+	/**
+	 * Draw a string
+	 * 
+	 * @param x
+	 *            The x position to draw the string
+	 * @param y
+	 *            The y position to draw the string
+	 * @param whatchars
+	 *            The string to draw
+	 */
+	public void drawString(float x, float y, String whatchars) {
+		drawString(x, y, whatchars, org.newdawn.slick.Color.white);
+	}
+
+}
---
+++ src/org/newdawn/slick/util/BufferedImageUtil.java
@@ -0,0 +1,195 @@
+package org.newdawn.slick.util;
+
+import java.awt.Color;
+import java.awt.Graphics;
+import java.awt.color.ColorSpace;
+import java.awt.image.BufferedImage;
+import java.awt.image.ColorModel;
+import java.awt.image.ComponentColorModel;
+import java.awt.image.DataBuffer;
+import java.awt.image.DataBufferByte;
+import java.awt.image.Raster;
+import java.awt.image.WritableRaster;
+import java.io.IOException;
+import java.nio.ByteBuffer;
+import java.nio.ByteOrder;
+import java.util.Hashtable;
+
+import org.lwjgl.opengl.GL11;
+import org.lwjgl.opengl.glu.GLU;
+import org.newdawn.slick.opengl.Texture;
+import org.newdawn.slick.opengl.TextureLoader;
+
+/**
+ * This is a utility class that allows you to convert a BufferedImage into a
+ * texture.
+ * 
+ * @author James Chambers (Jimmy)
+ * @author Jeremy Adams (elias_naur)
+ * @author Kevin Glass (kevglass)
+ */
+
+public class BufferedImageUtil {
+
+	/**
+	 * Load a texture
+	 * 
+	 * @param resourceName
+	 *            The location of the resource to load
+	 * @param resourceImage
+	 *            The BufferedImage we are converting
+	 * @return The loaded texture
+	 * @throws IOException
+	 *             Indicates a failure to access the resource
+	 */
+	public static Texture getTexture(String resourceName,
+			BufferedImage resourceImage) throws IOException {
+		Texture tex = getTexture(resourceName, resourceImage,
+				GL11.GL_TEXTURE_2D, // target
+				GL11.GL_RGBA, // dest pixel format
+				GL11.GL_LINEAR_MIPMAP_NEAREST, // min filter (unused)
+				GL11.GL_LINEAR);
+
+		return tex;
+	}
+
+	/**
+	 * Load a texture into OpenGL from a BufferedImage
+	 * 
+	 * @param resourceName
+	 *            The location of the resource to load
+	 * @param resourceimage
+	 *            The BufferedImage we are converting
+	 * @param target
+	 *            The GL target to load the texture against
+	 * @param dstPixelFormat
+	 *            The pixel format of the screen
+	 * @param minFilter
+	 *            The minimising filter
+	 * @param magFilter
+	 *            The magnification filter
+	 * @return The loaded texture
+	 * @throws IOException
+	 *             Indicates a failure to access the resource
+	 */
+	public static Texture getTexture(String resourceName,
+			BufferedImage resourceimage, int target, int dstPixelFormat,
+			int minFilter, int magFilter) throws IOException {
+		int srcPixelFormat = 0;
+
+		// create the texture ID for this texture
+		int textureID = TextureLoader.createTextureID();
+		Texture texture = new Texture(resourceName, target, textureID);
+
+		// Enable texturing
+		GL11.glEnable(GL11.GL_TEXTURE_2D);
+
+		// bind this texture
+		GL11.glBindTexture(target, textureID);
+
+		BufferedImage bufferedImage = resourceimage;
+		texture.setWidth(bufferedImage.getWidth());
+		texture.setHeight(bufferedImage.getHeight());
+
+		if (bufferedImage.getColorModel().hasAlpha()) {
+			srcPixelFormat = GL11.GL_RGBA;
+		} else {
+			srcPixelFormat = GL11.GL_RGB;
+		}
+
+		// convert that image into a byte buffer of texture data
+		ByteBuffer textureBuffer = convertImageData(bufferedImage, texture);
+
+		if (target == GL11.GL_TEXTURE_2D) {
+			GL11
+					.glTexParameteri(target, GL11.GL_TEXTURE_WRAP_S,
+							GL11.GL_REPEAT);
+			GL11
+					.glTexParameteri(target, GL11.GL_TEXTURE_WRAP_T,
+							GL11.GL_REPEAT);
+			GL11.glTexParameteri(target, GL11.GL_TEXTURE_MIN_FILTER, minFilter);
+			GL11.glTexParameteri(target, GL11.GL_TEXTURE_MAG_FILTER, magFilter);
+		}
+
+		GLU.gluBuild2DMipmaps(target, dstPixelFormat, TextureLoader
+				.get2Fold(bufferedImage.getWidth()), TextureLoader
+				.get2Fold(bufferedImage.getHeight()), srcPixelFormat,
+				GL11.GL_UNSIGNED_BYTE, textureBuffer);
+
+		return texture;
+	}
+
+	/**
+	 * Convert the buffered image to a texture
+	 * 
+	 * @param bufferedImage
+	 *            The image to convert to a texture
+	 * @param texture
+	 *            The texture to store the data into
+	 * @return A ByteBuffer containing the data
+	 */
+	private static ByteBuffer convertImageData(BufferedImage bufferedImage,
+			Texture texture) {
+
+		/** The colour model including alpha for the GL image */
+		ColorModel glAlphaColorModel = new ComponentColorModel(ColorSpace
+				.getInstance(ColorSpace.CS_sRGB), new int[] { 8, 8, 8, 8 },
+				true, false, ComponentColorModel.BITMASK, DataBuffer.TYPE_BYTE);
+
+		/** The colour model for the GL image */
+		ColorModel glColorModel = new ComponentColorModel(ColorSpace
+				.getInstance(ColorSpace.CS_sRGB), new int[] { 8, 8, 8, 0 },
+				false, false, ComponentColorModel.OPAQUE, DataBuffer.TYPE_BYTE);
+
+		ByteBuffer imageBuffer = null;
+		WritableRaster raster;
+		BufferedImage texImage;
+
+		int texWidth = 2;
+		int texHeight = 2;
+
+		// find the closest power of 2 for the width and height
+		// of the produced texture
+		while (texWidth < bufferedImage.getWidth()) {
+			texWidth *= 2;
+		}
+		while (texHeight < bufferedImage.getHeight()) {
+			texHeight *= 2;
+		}
+
+		texture.setTextureHeight(texHeight);
+		texture.setTextureWidth(texWidth);
+
+		// create a raster that can be used by OpenGL as a source
+		// for a texture
+		if (bufferedImage.getColorModel().hasAlpha()) {
+			raster = Raster.createInterleavedRaster(DataBuffer.TYPE_BYTE,
+					texWidth, texHeight, 4, null);
+			texImage = new BufferedImage(glAlphaColorModel, raster, false,
+					new Hashtable());
+		} else {
+			raster = Raster.createInterleavedRaster(DataBuffer.TYPE_BYTE,
+					texWidth, texHeight, 3, null);
+			texImage = new BufferedImage(glColorModel, raster, false,
+					new Hashtable());
+		}
+
+		// copy the source image into the produced image
+		Graphics g = texImage.getGraphics();
+		g.setColor(new Color(0f, 0f, 0f, 0f));
+		g.fillRect(0, 0, texWidth, texHeight);
+		g.drawImage(bufferedImage, 0, 0, null);
+
+		// build a byte buffer from the temporary image
+		// that be used by OpenGL to produce a texture.
+		byte[] data = ((DataBufferByte) texImage.getRaster().getDataBuffer())
+				.getData();
+
+		imageBuffer = ByteBuffer.allocateDirect(data.length);
+		imageBuffer.order(ByteOrder.nativeOrder());
+		imageBuffer.put(data, 0, data.length);
+		imageBuffer.flip();
+
+		return imageBuffer;
+	}
+}
