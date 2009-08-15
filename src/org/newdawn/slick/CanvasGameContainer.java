075b24dc23a4a1730bb1c33710598a92bccaf50f
kevin <kevin@825ea778-2522-0410-b336-eb26e3730c59>
2009-08-15 10:03:12 +0000

    Make shared context optional for CanvasGameContainer

--- src/org/newdawn/slick/CanvasGameContainer.java
+++ src/org/newdawn/slick/CanvasGameContainer.java
@@ -26,6 +26,18 @@ public class CanvasGameContainer extends Canvas {
 	 * @throws SlickException Indicates a failure during creation of the container
 	 */
 	public CanvasGameContainer(Game game) throws SlickException {
+		this(game, false);
+	}
+	
+	/**
+	 * Create a new panel
+	 * 
+	 * @param game The game being held
+	 * @param shared True if shared GL context should be enabled. This allows multiple panels
+	 * to share textures and other GL resources.
+	 * @throws SlickException Indicates a failure during creation of the container
+	 */
+	public CanvasGameContainer(Game game, boolean shared) throws SlickException {
 		super();
 
 		this.game = game;
@@ -33,7 +45,7 @@ public class CanvasGameContainer extends Canvas {
 		requestFocus();
 		setSize(500,500);
 		
-		container = new Container(game);
+		container = new Container(game, shared);
 		container.setForceExit(false);
 	}
 
@@ -110,15 +122,19 @@ public class CanvasGameContainer extends Canvas {
 		 * 
 		 * @param game
 		 *            The game to be held in this container
+		 * @param shared True if shared GL context should be enabled. This allows multiple panels
+		 * to share textures and other GL resources.
 		 * @throws SlickException Indicates a failure to initialise
 		 */
-		public Container(Game game) throws SlickException {
+		public Container(Game game, boolean shared) throws SlickException {
 			super(game, CanvasGameContainer.this.getWidth(), CanvasGameContainer.this.getHeight(), false);
 
 			width = CanvasGameContainer.this.getWidth();
 			height = CanvasGameContainer.this.getHeight();
 			
-			enableSharedContext();
+			if (shared) {
+				enableSharedContext();
+			}
 		}
 
 		/**
